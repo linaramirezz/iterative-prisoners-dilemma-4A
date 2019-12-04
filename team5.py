31fb8f46dd026e585dac077cc5f37d6d92a51fc7
linaramirezz <58440585+linaramirezz@users.noreply.github.com>
2019-12-04 13:58:01 -0500

    Update team5.py
    hgbt

--- team5.py
+++ team5.py
@@ -1,3 +1,4 @@
+import random
 ####
 # Each team's file must define four tokens:
 #     team_name: a string
@@ -6,9 +7,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = Team Yoda
+strategy_name = Aleatoria
+strategy_description = 50/50 chance between betray and collude.
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -25,8 +26,7 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
+    return random.choice(["b", "c"])
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
